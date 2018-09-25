43f52a3b70588ef7a7c5d5f3c34667889de7f52e
manisero <michalaniserowicz@gmail.com>
2018-09-25 23:13:27 +0200

    Implemented creating Datasets

--- src/Manisero.StreamProcessing.DbSeeder/DatasetCreation/DatasetsCreator.cs
+++ src/Manisero.StreamProcessing.DbSeeder/DatasetCreation/DatasetsCreator.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Manisero.StreamProcessing.Domain;
+using Manisero.StreamProcessing.Utils;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace Manisero.StreamProcessing.DbSeeder.DatasetCreation
 {
@@ -8,7 +13,43 @@ namespace Manisero.StreamProcessing.DbSeeder.DatasetCreation
         public static IDictionary<short, Dataset> Create(
             string connectionString)
         {
-            return new Dictionary<short, Dataset>(); // TODO
+            var datasets = new Dictionary<short, Dataset>();
+
+            for (short datasetId = 1; datasetId <= 9; datasetId++)
+            {
+                datasets.Add(
+                    datasetId,
+                    new Dataset
+                    {
+                        DatasetId = datasetId,
+                        Date = new DateTime(2018, 1, 1).AddMonths(datasetId - 1)
+                    });
+            }
+
+            using (var connection = new NpgsqlConnection(connectionString).OpenIfClosed())
+            using (var writer = connection.BeginBinaryImport(GetCopyCommand<Dataset>()))
+            {
+                foreach (var set in datasets.Values)
+                {
+                    writer.StartRow();
+                    writer.Write(set.DatasetId, NpgsqlDbType.Smallint);
+                    writer.Write(set.Date, NpgsqlDbType.Date);
+                }
+
+                writer.Complete();
+            }
+
+            return datasets;
+        }
+
+        private static string GetCopyCommand<TEntity>()
+        {
+            var entityType = typeof(TEntity);
+
+            var columns = entityType.GetProperties().Select(x => x.Name);
+            var columnsString = string.Join(", ", columns.Select(x => $"\"{x}\""));
+
+            return $"COPY \"{entityType.Name}\" ({columnsString}) FROM STDIN BINARY;";
         }
     }
 }
---
+++ src/Manisero.StreamProcessing.Utils/NpgsqlConnectionUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace Manisero.StreamProcessing.Utils
+{
+    public static class NpgsqlConnectionUtils
+    {
+        public static NpgsqlConnection OpenIfClosed(
+            this NpgsqlConnection connection)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            return connection;
+        }
+    }
+}
