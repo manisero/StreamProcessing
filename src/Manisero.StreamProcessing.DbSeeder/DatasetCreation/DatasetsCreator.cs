3bb2195b32e589ff72ca8e674ccaecdd2eb5f825
manisero <michalaniserowicz@gmail.com>
2018-09-25 23:25:20 +0200

    Extracted PostgresCopyExecutor

--- src/Manisero.StreamProcessing.DbSeeder/DatasetCreation/DatasetsCreator.cs
+++ src/Manisero.StreamProcessing.DbSeeder/DatasetCreation/DatasetsCreator.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Manisero.StreamProcessing.Domain;
 using Manisero.StreamProcessing.Utils;
 using Npgsql;
@@ -10,6 +9,13 @@ namespace Manisero.StreamProcessing.DbSeeder.DatasetCreation
 {
     public static class DatasetsCreator
     {
+        private static readonly Dictionary<string, Action<NpgsqlBinaryImporter, Dataset>> ColumnMapping =
+            new Dictionary<string, Action<NpgsqlBinaryImporter, Dataset>>
+            {
+                [nameof(Dataset.DatasetId)] = (writer, x) => writer.Write(x.DatasetId, NpgsqlDbType.Smallint),
+                [nameof(Dataset.Date)] = (writer, x) => writer.Write(x.Date, NpgsqlDbType.Date)
+            };
+
         public static IDictionary<short, Dataset> Create(
             string connectionString)
         {
@@ -26,30 +32,15 @@ namespace Manisero.StreamProcessing.DbSeeder.DatasetCreation
                     });
             }
 
-            using (var connection = new NpgsqlConnection(connectionString).OpenIfClosed())
-            using (var writer = connection.BeginBinaryImport(GetCopyCommand<Dataset>()))
+            using (var connection = new NpgsqlConnection(connectionString))
             {
-                foreach (var set in datasets.Values)
-                {
-                    writer.StartRow();
-                    writer.Write(set.DatasetId, NpgsqlDbType.Smallint);
-                    writer.Write(set.Date, NpgsqlDbType.Date);
-                }
-
-                writer.Complete();
+                PostgresCopyExecutor.Execute(
+                    connection,
+                    datasets.Values,
+                    ColumnMapping);
             }
 
             return datasets;
-        }
-
-        private static string GetCopyCommand<TEntity>()
-        {
-            var entityType = typeof(TEntity);
-
-            var columns = entityType.GetProperties().Select(x => x.Name);
-            var columnsString = string.Join(", ", columns.Select(x => $"\"{x}\""));
-
-            return $"COPY \"{entityType.Name}\" ({columnsString}) FROM STDIN BINARY;";
         }
     }
 }
---
+++ src/Manisero.StreamProcessing.Utils/PostgresCopyExecutor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Npgsql;
+
+namespace Manisero.StreamProcessing.Utils
+{
+    public static class PostgresCopyExecutor
+    {
+        public static void Execute<TRow>(
+            NpgsqlConnection connection,
+            IEnumerable<TRow> rows,
+            IDictionary<string, Action<NpgsqlBinaryImporter, TRow>> columnMapping)
+        {
+            var copyCommand = GetCopyCommand(typeof(TRow).Name, columnMapping.Keys);
+            
+            using (var writer = connection.OpenIfClosed().BeginBinaryImport(copyCommand))
+            {
+                foreach (var row in rows)
+                {
+                    writer.StartRow();
+
+                    foreach (var column in columnMapping)
+                    {
+                        column.Value(writer, row);
+                    }
+                }
+
+                writer.Complete();
+            }
+        }
+
+        private static string GetCopyCommand(
+            string tableName,
+            ICollection<string> columnNames)
+        {
+            var columnsString = string.Join(", ", columnNames.Select(x => $"\"{x}\""));
+
+            return $"COPY \"{tableName}\" ({columnsString}) FROM STDIN BINARY;";
+        }
+    }
+}
