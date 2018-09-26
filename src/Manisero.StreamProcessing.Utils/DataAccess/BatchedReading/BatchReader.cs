121a1f1f47e0149bb6c4899acdeb03517896fff4
manisero <michalaniserowicz@gmail.com>
2018-09-26 23:43:40 +0200

    Implementing BatchReader

--- src/Manisero.StreamProcessing.Utils/DataAccess/BatchedReading/BatchReader.cs
+++ src/Manisero.StreamProcessing.Utils/DataAccess/BatchedReading/BatchReader.cs
@@ -12,7 +12,26 @@ namespace Manisero.StreamProcessing.Utils.DataAccess.BatchedReading
             int batchSize,
             IDictionary<string, int> filter = null)
         {
+            var whereClause = FormatWhereClause();
+            var orderClause = FormatOrderClause();
+
+            var query =
+$@"SELECT TOP({batchSize}) *
+FROM {tableName}
+WHERE {whereClause}
+ORDER BY {orderClause}";
+
             return new TRow[0];
+        }
+
+        private static string FormatWhereClause()
+        {
+            return null;
+        }
+
+        private static string FormatOrderClause()
+        {
+            return null;
         }
     }
 }
