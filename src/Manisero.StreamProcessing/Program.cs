f3cda3ac823919d2d9a6e6809801db39af9eb000
manisero <michalaniserowicz@gmail.com>
2018-09-27 22:54:02 +0200

    Cleanup

--- src/Manisero.StreamProcessing/Program.cs
+++ src/Manisero.StreamProcessing/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Manisero.StreamProcessing.Process;
+﻿using Manisero.StreamProcessing.Process;
 using Manisero.StreamProcessing.Process.DataAccess;
 using Manisero.StreamProcessing.Process.LoansProcessing;
 using Manisero.StreamProcessing.Utils;
@@ -14,17 +13,7 @@ namespace Manisero.StreamProcessing
             var connectionString = config.GetDefaultConnectionString();
 
             var clientRepository = new ClientRepository(connectionString);
-            var clientCount = clientRepository.CountInDataset(5);
-            var clientReader = clientRepository.GetBatchedReader(5);
-            var clientsBatch1 = clientReader.ReadNext();
-            var clientsBatch2 = clientReader.ReadNext();
-
             var loanRepository = new LoanRepository(connectionString);
-            var loanReader = loanRepository.GetBatchedReader(5);
-            var loansBatch1 = loanReader.ReadNext();
-            var loansBatch2 = loanReader.ReadNext();
-
-            var loansRange = loanRepository.GetRange(5, clientsBatch2.First().ClientId, clientsBatch2.Last().ClientId);
 
             var taskExecutorFactory = new TaskExecutorFactory();
             var taskExecutor = taskExecutorFactory.Create();
