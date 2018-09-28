be3495535576c4b379d0dd961812214e38557276
manisero <michalaniserowicz@gmail.com>
2018-09-28 08:40:48 +0200

    Implementing TaskExecutionLog

---
+++ src/Manisero.StreamProcessing.Process/TaskExecutionLog.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Manisero.StreamProcessing.Process
+{
+    public static class TaskExecutionLog
+    {
+        public class DurationLog
+        {
+            public DateTime StartTs { get; set; }
+
+            public DateTime EndTs { get; set; }
+
+            public TimeSpan Duration { get; set; }
+
+            public void SetEnd(
+                DateTime endTs,
+                TimeSpan duration)
+            {
+                EndTs = endTs;
+                Duration = duration;
+            }
+        }
+
+        public class ItemLog
+        {
+            public DurationLog Duration { get; set; } = new DurationLog();
+
+            public TimeSpan MaterializationDuration { get; set; }
+
+            public ConcurrentDictionary<string, DurationLog> BlockDurations { get; set; } = new ConcurrentDictionary<string, DurationLog>();
+
+            public void SetStart(
+                DateTime startTs,
+                TimeSpan materializationDuration)
+            {
+                Duration.StartTs = startTs;
+                MaterializationDuration = materializationDuration;
+            }
+
+            public void StartBlock(
+                string name,
+                DateTime startTs)
+            {
+                BlockDurations.GetOrAdd(name, x => new DurationLog()).StartTs = startTs;
+            }
+        }
+
+        public class BlockTotalsLog
+        {
+            public TimeSpan MaterializationDuration { get; set; }
+
+            public Dictionary<string, TimeSpan> BlockDurations { get; set; }
+        }
+
+        public class StepLog
+        {
+            public DurationLog Duration { get; set; } = new DurationLog();
+
+            /// <summary>ItemNumber -> Log</summary>
+            public ConcurrentDictionary<int, ItemLog> ItemLogs { get; set; } = new ConcurrentDictionary<int, ItemLog>();
+
+            public BlockTotalsLog BlockTotals { get; set; }
+
+            public void StartItem(
+                int number,
+                DateTime startTs,
+                TimeSpan materializationDuration)
+            {
+                ItemLogs.GetOrAdd(number, x => new ItemLog()).SetStart(startTs, materializationDuration);
+            }
+        }
+
+        public static DurationLog TaskDuration { get; set; }
+
+        /// <summary>StepName -> Log</summary>
+        public static ConcurrentDictionary<string, StepLog> StepLogs { get; set; }
+
+        public static void Reset(
+            DateTime taskStartTs)
+        {
+            TaskDuration = new DurationLog { StartTs = taskStartTs };
+            StepLogs = new ConcurrentDictionary<string, StepLog>();
+        }
+
+        public static void StartStep(
+            string name,
+            DateTime startTs)
+        {
+            StepLogs.GetOrAdd(name, x => new StepLog()).Duration.StartTs = startTs;
+        }
+    }
+}
--- src/Manisero.StreamProcessing.Process/TaskExecutorFactory.cs
+++ src/Manisero.StreamProcessing.Process/TaskExecutorFactory.cs
@@ -1,4 +1,9 @@
-﻿using Manisero.Navvy;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Manisero.Navvy;
+using Manisero.Navvy.Core;
 using Manisero.Navvy.Core.Events;
 using Manisero.Navvy.Dataflow;
 using Manisero.Navvy.PipelineProcessing.Events;
@@ -8,19 +13,34 @@ namespace Manisero.StreamProcessing.Process
     public interface ITaskExecutorFactory
     {
         ITaskExecutor Create(
-            TaskExecutionEvents taskEvents,
-            PipelineExecutionEvents pipelineEvents);
+            params IExecutionEvents[] events);
     }
 
     public class TaskExecutorFactory : ITaskExecutorFactory
     {
         public ITaskExecutor Create(
-            TaskExecutionEvents taskEvents,
-            PipelineExecutionEvents pipelineEvents)
+            params IExecutionEvents[] events)
         {
+            var taskEvents = new TaskExecutionEvents(
+                taskStarted: x => TaskExecutionLog.Reset(x.Timestamp),
+                taskEnded: x => TaskExecutionLog.TaskDuration.EndTs = x.Timestamp,
+                stepStarted: x => TaskExecutionLog.StartStep(x.Step.Name, x.Timestamp),
+                stepEnded: x => TaskExecutionLog.StepLogs[x.Step.Name].Duration.SetEnd(x.Timestamp, x.Duration));
+
+            var pipelineEvents = new PipelineExecutionEvents(
+                itemStarted: x => TaskExecutionLog.StepLogs[x.Step.Name].StartItem(x.ItemNumber, x.Timestamp, x.MaterializationDuration),
+                itemEnded: x => TaskExecutionLog.StepLogs[x.Step.Name].ItemLogs[x.ItemNumber].Duration.SetEnd(x.Timestamp, x.Duration),
+                blockStarted: x => TaskExecutionLog.StepLogs[x.Step.Name].ItemLogs[x.ItemNumber].StartBlock(x.Block.Name, x.Timestamp),
+                blockEnded: x => TaskExecutionLog.StepLogs[x.Step.Name].ItemLogs[x.ItemNumber].BlockDurations[x.Block.Name].SetEnd(x.Timestamp, x.Duration),
+                pipelineEnded: x => TaskExecutionLog.StepLogs[x.Step.Name].BlockTotals = new TaskExecutionLog.BlockTotalsLog
+                {
+                    MaterializationDuration = x.TotalInputMaterializationDuration,
+                    BlockDurations = x.TotalBlockDurations.ToDictionary(entry => entry.Key, entry => entry.Value)
+                });
+
             return new TaskExecutorBuilder()
                 //.RegisterDataflowExecution()
-                .RegisterEvents(taskEvents, pipelineEvents)
+                .RegisterEvents(events)
                 .Build();
         }
     }
