a49ef5190e315896739d59b7a5352354be130625
manisero <michalaniserowicz@gmail.com>
2018-10-03 07:57:33 +0200

    Extracting Manisero.Navvy.Reporting project

---
+++ src/Manisero.Navvy.Reporting/TaskExecutionLog.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Manisero.Navvy.Reporting
+{
+    public class TaskExecutionLog
+    {
+        public TaskDefinition Task { get; set; }
+
+        public DurationLog TaskDuration { get; } = new DurationLog();
+
+        /// <summary>StepName -> Log</summary>
+        public ConcurrentDictionary<string, TaskStepLog> StepLogs { get; } = new ConcurrentDictionary<string, TaskStepLog>();
+    }
+
+    public class TaskStepLog
+    {
+        public DurationLog Duration { get; } = new DurationLog();
+
+        /// <summary>ItemNumber -> Log</summary>
+        public ConcurrentDictionary<int, PipelineItemLog> ItemLogs { get; } = new ConcurrentDictionary<int, PipelineItemLog>();
+
+        public PipelineBlockTotalsLog BlockTotals { get; set; }
+    }
+
+    public class PipelineItemLog
+    {
+        public DurationLog Duration { get; } = new DurationLog();
+
+        public TimeSpan MaterializationDuration { get; set; }
+
+        public ConcurrentDictionary<string, DurationLog> BlockDurations { get; } = new ConcurrentDictionary<string, DurationLog>();
+    }
+
+    public class PipelineBlockTotalsLog
+    {
+        public TimeSpan MaterializationDuration { get; set; }
+
+        public Dictionary<string, TimeSpan> BlockDurations { get; set; }
+    }
+
+    public class DurationLog
+    {
+        public DateTime StartTs { get; private set; }
+
+        public DateTime EndTs { get; private set; }
+
+        public TimeSpan Duration { get; private set; }
+
+        public void SetStart(
+            DateTime startTs)
+        {
+            StartTs = startTs;
+        }
+
+        public void SetEnd(
+            DateTime endTs,
+            TimeSpan duration)
+        {
+            EndTs = endTs;
+            Duration = duration;
+        }
+    }
+}
---
+++ src/Manisero.Navvy.Reporting/TaskExecutionLogger.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using Manisero.Navvy.Core.Events;
+using Manisero.Navvy.PipelineProcessing.Events;
+
+namespace Manisero.Navvy.Reporting
+{
+    public class TaskExecutionLogger
+    {
+        public void HandleTaskStarted(
+            TaskStartedEvent e, TaskExecutionLog log)
+        {
+            log.Task = e.Task;
+            log.TaskDuration.SetStart(e.Timestamp);
+        }
+
+        public void HandleTaskEnded(
+            TaskEndedEvent e, TaskExecutionLog log)
+        {
+            log.TaskDuration.SetEnd(e.Timestamp, e.Duration);
+        }
+
+        public void HandleStepStarted(
+            StepStartedEvent e, TaskExecutionLog log)
+        {
+            var stepLog = new TaskStepLog();
+            stepLog.Duration.SetStart(e.Timestamp);
+            log.StepLogs[e.Step.Name] = stepLog;
+        }
+
+        public void HandleStepEnded(
+            StepEndedEvent e, TaskExecutionLog log)
+        {
+            log.StepLogs[e.Step.Name].Duration.SetEnd(e.Timestamp, e.Duration);
+        }
+
+        public void HandleItemMaterialized(
+            ItemMaterializedEvent e, TaskExecutionLog log)
+        {
+            var itemLog = new PipelineItemLog();
+            itemLog.Duration.SetStart(e.ItemStartTimestamp);
+            itemLog.MaterializationDuration = e.MaterializationDuration;
+            
+            log.StepLogs[e.Step.Name].ItemLogs[e.ItemNumber] = itemLog;
+        }
+
+        public void HandleItemEnded(
+            ItemEndedEvent e, TaskExecutionLog log)
+        {
+            log.StepLogs[e.Step.Name].ItemLogs[e.ItemNumber].Duration.SetEnd(e.Timestamp, e.Duration);
+        }
+
+        public void HandleBlockStarted(
+            BlockStartedEvent e, TaskExecutionLog log)
+        {
+            var blockLog = new DurationLog();
+            blockLog.SetStart(e.Timestamp);
+
+            log.StepLogs[e.Step.Name].ItemLogs[e.ItemNumber].BlockDurations[e.Block.Name] = blockLog;
+        }
+
+        public void HandleBlockEnded(
+            BlockEndedEvent e, TaskExecutionLog log)
+        {
+            log.StepLogs[e.Step.Name].ItemLogs[e.ItemNumber].BlockDurations[e.Block.Name].SetEnd(e.Timestamp, e.Duration);
+        }
+
+        public void HandlePipelineEnded(
+            PipelineEndedEvent e, TaskExecutionLog log)
+        {
+            log.StepLogs[e.Step.Name].BlockTotals = new PipelineBlockTotalsLog
+            {
+                MaterializationDuration = e.TotalInputMaterializationDuration,
+                BlockDurations = e.TotalBlockDurations.ToDictionary(entry => entry.Key, entry => entry.Value)
+            };
+        }
+    }
+}
