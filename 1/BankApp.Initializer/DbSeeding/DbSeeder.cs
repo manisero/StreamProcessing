0b1d290ea099e09f539534ac6ffeb717f00e8048
manisero <michalaniserowicz@gmail.com>
2018-10-07 10:43:21 +0200

    Added Initializer project

---
+++ 1/BankApp.Initializer/DbSeeding/DbSeeder.cs
@@ -0,0 +1,11 @@
+﻿namespace BankApp.Initializer.DbSeeding
+{
+    public static class DbSeeder
+    {
+        public static void Seed(
+            string connectionString)
+        {
+
+        }
+    }
+}
---
+++ 1/BankApp.Initializer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using BankApp.DataAccess;
+using BankApp.Initializer.DbSeeding;
+using BankApp.Utils;
+
+namespace BankApp.Initializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigUtils.GetConfig();
+            var connectionString = config.GetDefaultConnectionString();
+
+            Console.WriteLine("Ensuring db created...");
+            var efContext = new EfContext(connectionString);
+            efContext.Database.EnsureCreated();
+
+            Console.WriteLine("Seeding db...");
+            var seedSw = Stopwatch.StartNew();
+            DbSeeder.Seed(connectionString);
+            Console.WriteLine($"Seeding db took {seedSw.Elapsed}.");
+        }
+    }
+}
