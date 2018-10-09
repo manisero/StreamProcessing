5818e2cc8af8bc069bac00c773bcfdd44f577a4a
manisero <michalaniserowicz@gmail.com>
2018-10-09 21:05:39 +0200

    Added ClientTotalLoanRepository

---
+++ 1/BankApp.DataAccess/ClientTotalLoanRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using BankApp.Domain;
+using BankApp.Utils.DatabaseAccess;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace BankApp.DataAccess
+{
+    public class ClientTotalLoanRepository
+    {
+        private static readonly Dictionary<string, Action<NpgsqlBinaryImporter, ClientTotalLoan>> ColumnMapping =
+            new Dictionary<string, Action<NpgsqlBinaryImporter, ClientTotalLoan>>
+            {
+                [nameof(ClientTotalLoan.ClientLoansCalculationId)] = (writer, x) => writer.Write(x.ClientLoansCalculationId, NpgsqlDbType.Integer),
+                [nameof(ClientTotalLoan.ClientId)] = (writer, x) => writer.Write(x.ClientId, NpgsqlDbType.Integer),
+                [nameof(ClientTotalLoan.TotalLoan)] = (writer, x) => writer.Write(x.TotalLoan, NpgsqlDbType.Numeric)
+            };
+
+        private readonly string _connectionString;
+
+        public ClientTotalLoanRepository(
+            string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void CreateMany(
+            IEnumerable<ClientTotalLoan> items)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                PostgresCopyExecutor.Execute(
+                    connection,
+                    items,
+                    ColumnMapping);
+            }
+        }
+    }
+}
