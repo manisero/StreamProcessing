0523d425f0c9c95d3f8cc248b2d4f34d21b6c18e
manisero <michalaniserowicz@gmail.com>
2018-10-09 07:49:34 +0200

    Replaced CalculateClientLoansTask blocks with BasicSteps

--- 1/BankApp/CalculateClientLoans/CalculateClientLoansTaskFactory.cs
+++ 1/BankApp/CalculateClientLoans/CalculateClientLoansTaskFactory.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using BankApp.DataAccess;
 using BankApp.Domain;
 using Manisero.Navvy;
-using Manisero.Navvy.PipelineProcessing;
+using Manisero.Navvy.BasicProcessing;
 using Microsoft.EntityFrameworkCore;
 
 namespace BankApp.CalculateClientLoans
@@ -23,24 +22,49 @@ namespace BankApp.CalculateClientLoans
             int datasetId)
         {
             var clientLoansCalculation = CreateClientLoansCalculation(datasetId);
+            var datasetToProcess = new DatasetToProcess();
 
             return new TaskDefinition(
                 $"{nameof(CalculateClientLoans)}_{clientLoansCalculation.ClientLoansCalculationId}",
-                PipelineTaskStep.Builder<DatasetToProcess>(nameof(CalculateClientLoans))
-                    .WithInput(LoadDatasetAsEnumerable(datasetId), 1)
-                    .WithBlock(
-                        "Calculate",
-                        x =>
+                new BasicTaskStep(
+                    "LoadDataset",
+                    () =>
+                    {
+                        using (var context = _efContextFactory())
                         {
-                            foreach (var client in x.Dataset.Clients)
-                            {
-                                x.ClientLoans.Add(client.ClientId, client.Loans.Sum(l => l.Value));
-                            }
-                        })
-                    .WithBlock(
-                        "SaveClientTotalLoans",
-                        x => SaveClientTotalLoans(x, clientLoansCalculation.ClientLoansCalculationId))
-                    .Build());
+                            datasetToProcess.Dataset = context
+                                .Set<Dataset>()
+                                .Include(x => x.Clients).ThenInclude(x => x.Loans)
+                                .Single(x => x.DatasetId == datasetId);
+                        }
+                    }),
+                new BasicTaskStep(
+                    "CalculateTotalLoans",
+                    () =>
+                    {
+                        foreach (var client in datasetToProcess.Dataset.Clients)
+                        {
+                            datasetToProcess.ClientLoans.Add(client.ClientId, client.Loans.Sum(l => l.Value));
+                        }
+                    }),
+                new BasicTaskStep(
+                    "SaveClientTotalLoans",
+                    () =>
+                    {
+                        using (var context = _efContextFactory())
+                        {
+                            var clientTotalLoans = datasetToProcess.ClientLoans.Select(
+                                x => new ClientTotalLoan
+                                {
+                                    ClientLoansCalculationId = clientLoansCalculation.ClientLoansCalculationId,
+                                    ClientId = x.Key,
+                                    TotalLoan = x.Value
+                                });
+
+                            context.Set<ClientTotalLoan>().AddRange(clientTotalLoans);
+                            context.SaveChanges();
+                        }
+                    }));
         }
 
         private ClientLoansCalculation CreateClientLoansCalculation(
@@ -57,39 +81,6 @@ namespace BankApp.CalculateClientLoans
                 context.SaveChanges();
 
                 return clientLoansCalculation;
-            }
-        }
-
-        private IEnumerable<DatasetToProcess> LoadDatasetAsEnumerable(
-            int datasetId)
-        {
-            using (var context = _efContextFactory())
-            {
-                var dataset = context
-                    .Set<Dataset>()
-                    .Include(x => x.Clients).ThenInclude(x => x.Loans)
-                    .Single(x => x.DatasetId == datasetId);
-
-                yield return new DatasetToProcess { Dataset = dataset };
-            }
-        }
-
-        private void SaveClientTotalLoans(
-            DatasetToProcess datasetToProcess,
-            int clientLoansCalculationId)
-        {
-            using (var context = _efContextFactory())
-            {
-                var clientTotalLoans = datasetToProcess.ClientLoans.Select(
-                    x => new ClientTotalLoan
-                    {
-                        ClientLoansCalculationId = clientLoansCalculationId,
-                        ClientId = x.Key,
-                        TotalLoan = x.Value
-                    });
-
-                context.Set<ClientTotalLoan>().AddRange(clientTotalLoans);
-                context.SaveChanges();
             }
         }
     }
