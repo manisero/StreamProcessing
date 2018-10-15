af1a535cbb1ecbf3c2d306f8c2ac236ad4a9e400
manisero <michalaniserowicz@gmail.com>
2018-10-15 23:33:52 +0200

    Migrating StreamProcessing to BankApp

---
+++ 1/BankApp8.Init/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApp8.Init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
