796c5ec7e984db527f650280ea962480dd3b74f7
manisero <michalaniserowicz@gmail.com>
2018-10-17 23:01:48 +0200

    Adding BankApp3

---
+++ src/BankApp3.Init/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApp3.Init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/BankApp3.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApp3.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
