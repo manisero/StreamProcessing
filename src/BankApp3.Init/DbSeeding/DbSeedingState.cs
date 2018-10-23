4beed47bda4cf24fe54f98c6e8bdfc29cec92ab2
manisero <michalaniserowicz@gmail.com>
2018-10-23 07:24:35 +0200

    Removed BankApp3.Init

--- src/BankApp3.Init/DbSeeding/DbSeedingState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace BankApp3.Init.DbSeeding
-{
-    public class DbSeedingState
-    {
-        public ICollection<short> DatasetIds { get; set; }
-    }
-}
