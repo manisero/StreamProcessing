33dfb05cb975e6832507fe7fa25925db3654511c
manisero <michalaniserowicz@gmail.com>
2018-10-25 20:02:10 +0200

    Added ClientLoansCalculator3

---
+++ src/ClientLoansCalculator3/Program.cs
@@ -0,0 +1,27 @@
+﻿using BankApp.DataAccess.WideKeys.Data;
+using BankApp.DataAccess.WideKeys.Tasks;
+using ClientLoansCalculator1;
+using DataProcessing.Utils;
+using DataProcessing.Utils.Navvy;
+
+namespace ClientLoansCalculator3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = ConfigUtils.GetAppSettings();
+            var taskExecutor = TaskExecutorFactory.Create();
+
+            var taskFactory = new ClientLoansCalculationTaskFactory(
+                new LoanSnapshotRepository(settings.ConnectionString),
+                new ClientLoansCalculationRepository(settings.ConnectionString),
+                new ClientTotalLoanRepository(settings.ConnectionString));
+
+            var datasetId = new DatasetRepository(settings.ConnectionString).GetMaxId();
+
+            var task = taskFactory.Create(datasetId.Value);
+            taskExecutor.Execute(task);
+        }
+    }
+}
