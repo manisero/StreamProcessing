1e8cd8960c0cfc5c79a0609f51d7d772341fd410
manisero <michalaniserowicz@gmail.com>
2018-10-29 23:10:48 +0100

    Implementing ClientLoansCalculationDeleter1

---
+++ src/ClientLoansCalculationDeleter1/Program.cs
@@ -0,0 +1,15 @@
+﻿using BankApp.DataAccess.WideKeys.Tasks;
+using DataProcessing.Utils;
+
+namespace ClientLoansCalculationDeleter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = ConfigUtils.GetAppSettings();
+
+            var clientLoansCalculationRepository = new ClientLoansCalculationRepository(settings.ConnectionString);
+        }
+    }
+}
