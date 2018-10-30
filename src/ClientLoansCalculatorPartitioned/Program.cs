587929c9cfc23111c3f02845eb5038705054524e
manisero <michalaniserowicz@gmail.com>
2018-10-30 19:28:10 +0100

    Added ClientLoansCalculatorPartitioned

---
+++ src/ClientLoansCalculatorPartitioned/Program.cs
@@ -0,0 +1,30 @@
+﻿using BankApp.DataAccess.Partitioned.Data;
+using BankApp.DataAccess.Partitioned.Tasks;
+using ClientLoansCalculator1;
+using DataProcessing.Utils;
+using DataProcessing.Utils.DatabaseAccess;
+using DataProcessing.Utils.Navvy;
+
+namespace ClientLoansCalculatorPartitioned
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = ConfigUtils.GetAppSettings();
+            var databaseManager = new DatabaseManager(settings.ConnectionString);
+            var taskExecutor = TaskExecutorFactory.Create();
+            
+            var taskFactory = new ClientLoansCalculationTaskFactory(
+                new LoanSnapshotRepository(settings.ConnectionString),
+                new ClientLoansCalculationRepository(settings.ConnectionString, databaseManager),
+                new ClientTotalLoanRepository(settings.ConnectionString));
+
+            var datasetId = new DatasetRepository(settings.ConnectionString, databaseManager)
+                .GetMaxId();
+
+            var task = taskFactory.Create(datasetId.Value);
+            taskExecutor.Execute(task);
+        }
+    }
+}
